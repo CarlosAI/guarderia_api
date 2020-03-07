d3bd8d6c9f23fd8838447557145c97ffc312cc7b
CarlosAI <carloswesker@outlook.es>
2020-03-07 17:07:37 -0600

    bd

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_07_230556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "activity_logs", force: :cascade do |t|
+    t.integer "baby_id"
+    t.integer "assistant_id"
+    t.integer "activity_id"
+    t.date "start_time"
+    t.date "stop_time"
+    t.integer "duration"
+    t.text "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "assistants", force: :cascade do |t|
+    t.string "name"
+    t.string "group"
+    t.string "address"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "babies", force: :cascade do |t|
+    t.string "name"
+    t.date "birthday"
+    t.string "mother_name"
+    t.string "father_name"
+    t.string "address"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
